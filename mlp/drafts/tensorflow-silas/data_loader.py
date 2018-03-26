3cb4620ce041a893c4025e6af60919ea0da6f718
Silas Berger <berger.silu@hotmail.com>
2018-03-26 17:38:56 +0200

    started working on data_loader
    loading and one-hot conversion work

---
+++ mlp/drafts/tensorflow-silas/data_loader.py
@@ -0,0 +1,22 @@
+import csv
+import numpy as np
+
+
+def load_data(path):
+    with open(path, "r") as file:
+        data = []
+        labels = []
+        csvfile = csv.reader(file, delimiter=",", quoting=csv.QUOTE_NONNUMERIC)
+        for line in csvfile:
+            labels.append(line[0])
+            data.append(line[1:])
+    return np.array(data, dtype=np.float32), labels_to_one_hot(labels)
+
+
+def labels_to_one_hot(labels):
+    one_hot_labels = []
+    for label in labels:
+        one_hot_vector = np.zeros(10)
+        one_hot_vector[int(label)] = 1
+        one_hot_labels.append(one_hot_vector)
+    return np.array(one_hot_labels)
