7e4bba687bfa5c8f309fd31c912a235d071791ad
Pas <pascal_zaugg@web.de>
2018-04-14 13:34:39 +0200

    Saving all images as binarized versions.

--- 03 - DVM/runner.py
+++ 03 - DVM/runner.py
@@ -9,11 +9,14 @@ results = paths.get( "data/ground-truth/locations/270.svg" )
 
 imageP = ImageProcessor( "data/images/270.jpg" )
 
+preprocess = Preprocessor()
+
 number = 1
 
 for path in results:  
   new_image = imageP.crop( path )
-  new_image = Image.fromarray( new_image.astype( "uint8" ) )
+  new_image = preprocess.binarization( new_image )
+  new_image = Image.fromarray( new_image )
   new_image.save("images/image-"+str(number) +'.png')
   
   number += 1
