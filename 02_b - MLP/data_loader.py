a583d1ad38acc8620ce0125b87a7e3de5afe240d
piachristel <muellerchristina@bluewin.ch>
2018-05-23 19:55:30 +0200

    Add evaluatoin of MLP to MLP folder.

--- 02_b - MLP/data_loader.py
+++ 02_b - MLP/data_loader.py
@@ -26,6 +26,39 @@ def load_data(path, normalize=True):
         data = data / 255
     return data, _labels_to_one_hot(labels)
 
+def load_train_data(path, normalize=True):
+    """
+    Loads data from specified path, returns matrix of data points (in row-orientation) and matrix of the corresponding
+    10-class one-hot encoded labels. Data is normalized by default (assuming original scale 0-255).
+
+    :param path:        data path (e.g. 'data/train.csv')
+    :param normalize:   normalize data (bool)
+    :return:    np.ndarray with dimensions [num_data_rows, len_feature_vector] containing (normalized) data,
+                and np.ndarray with dimensions [num_data_rows, 10] containing one-hot encoded labels corresponding
+                to data.
+    """
+    with open(path, "r") as file:
+        data = []
+        labels = []
+        csvfile = csv.reader(file, delimiter=",", quoting=csv.QUOTE_NONNUMERIC)
+        for line in csvfile:
+            labels.append(line[0]) # TODO: add method for no label and mabye identifier here.
+            data.append(line[1:])
+    data = np.array(data, dtype=np.float32)
+    if normalize:
+        data = data / 255
+    return data, _labels_to_one_hot(labels)
+
+def load_test_data(path, normalize=True):
+    with open(path, "r") as file:
+        data = []
+        csvfile = csv.reader(file, delimiter=",", quoting=csv.QUOTE_NONNUMERIC)
+        for line in csvfile:
+            data.append(line[0:])
+    data = np.array(data, dtype=np.float32)
+    if normalize:
+        data = data / 255
+    return data
 
 def _labels_to_one_hot(labels):
     one_hot_labels = []
